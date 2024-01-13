6b40783c7d6046c7475e01af0f95f184866367e8
Faust <1546327522@qq.com>
2024-01-13 23:03:32 +0800

    feat: improve shutdown

--- src/db.rs
+++ src/db.rs
@@ -7,7 +7,8 @@ use std::{
     time::Duration,
 };
 use tokio::{
-    spawn,
+    select, spawn,
+    sync::{broadcast, mpsc},
     time::{sleep, Instant},
 };
 
@@ -22,17 +23,35 @@ pub struct SharedDb {
     database: Mutex<Database>,
 }
 
+struct DbCleaner {
+    db: Arc<SharedDb>,
+    shutdown_notifier: broadcast::Receiver<()>,
+    _shutdown_completed_tx: mpsc::Sender<()>,
+}
+
 struct Database {
     entries: HashMap<String, Bytes>,
     expiration: BTreeMap<String, Instant>,
 }
 
 impl DbHolder {
-    pub fn new() -> DbHolder {
+    pub fn new(
+        shutdown_notifier: broadcast::Receiver<()>,
+        shutdown_completed_tx: mpsc::Sender<()>,
+    ) -> DbHolder {
         let holder = Arc::new(SharedDb {
             database: Mutex::new(Database::new()),
         });
-        spawn(clean_expired_keys(holder.clone()));
+
+        let db = holder.clone();
+        spawn(async move {
+            let mut cleaner = DbCleaner {
+                db,
+                shutdown_notifier,
+                _shutdown_completed_tx: shutdown_completed_tx,
+            };
+            cleaner.clean_expired_keys().await;
+        });
         DbHolder { holder }
     }
 
@@ -80,12 +99,19 @@ impl SharedDb {
     }
 }
 
-async fn clean_expired_keys(db: Arc<SharedDb>) {
-    // TODO: end task when shutdown
-    loop {
-        let sleep_time = db.clean_expired_keys().unwrap_or(DEFAULT_SLEEP_TIME);
-        // TODO: wake up if a new key coming
-        sleep(sleep_time).await;
+impl DbCleaner {
+    async fn clean_expired_keys(&mut self) {
+        loop {
+            let sleep_time = self.db.clean_expired_keys().unwrap_or(DEFAULT_SLEEP_TIME);
+            select! {
+                _ = self.shutdown_notifier.recv() => {
+                    println!("background task stopped");
+                    return;
+                },
+                _ = sleep(sleep_time) => {},
+            }
+            // TODO: wake up if a new key coming
+        }
     }
 }
 
@@ -102,13 +128,22 @@ impl Database {
 mod tests {
 
     use super::*;
-    
+
     #[tokio::test]
     async fn clean_expired_keys_test() {
-        let dbhodler = DbHolder::new(); 
-        dbhodler.set("test".to_string(), Bytes::from_static(b"h"), Some(Duration::from_secs(1)));
+        let (shutdown_tx, _) = broadcast::channel(1);
+        let (shutdown_completed_tx, mut shutdown_completed_rx) = mpsc::channel(1);
+        let dbhodler = DbHolder::new(shutdown_tx.subscribe(), shutdown_completed_tx.clone());
+        dbhodler.set(
+            "test".to_string(),
+            Bytes::from_static(b"h"),
+            Some(Duration::from_secs(1)),
+        ).unwrap();
         assert_eq!(dbhodler.get("test"), Some(Bytes::from_static(b"h")));
         sleep(Duration::from_secs(2)).await;
         assert_eq!(dbhodler.get("test"), None);
+        shutdown_tx.send(()).unwrap();
+        drop(shutdown_completed_tx);
+        shutdown_completed_rx.recv().await;
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -4,7 +4,7 @@ use crate::{cmd::Command, connection::Connection, DbHolder, Error, Frame, Result
 use tokio::{
     net::TcpListener,
     select, spawn,
-    sync::{broadcast, Semaphore},
+    sync::{broadcast, mpsc, Semaphore},
 };
 
 const MAX_LIMIT_CONNECTIONS: usize = 10;
@@ -13,17 +13,21 @@ pub struct Listener {
     listener: TcpListener,
     semaphore: Arc<Semaphore>,
     shutdown_broadcast: broadcast::Sender<()>,
+    shutdown_completed_tx: mpsc::Sender<()>,
 }
 
 pub struct Handler {
     connection: Connection,
     db: DbHolder,
     shutdown_receiver: broadcast::Receiver<()>,
+    _shutdown_completed_tx: mpsc::Sender<()>,
 }
 
 pub async fn run(addr: &str, signal: impl Future) -> Result<()> {
     let (shutdown_tx, _) = broadcast::channel(1);
-    let mut listener = Listener::new(addr, shutdown_tx).await?;
+    let (shutdown_completed_tx, mut shutdown_completed_rx) = mpsc::channel(1);
+
+    let mut listener = Listener::new(addr, shutdown_tx, shutdown_completed_tx).await?;
 
     select! {
         res = listener.run() => {
@@ -36,24 +40,40 @@ pub async fn run(addr: &str, signal: impl Future) -> Result<()> {
         }
     }
 
-    listener.shutdown()?;
+    println!("send signal");
+    listener.shutdown_broadcast.send(()).unwrap();
+    let Listener {
+        shutdown_completed_tx,
+        ..
+    } = listener;
+    drop(shutdown_completed_tx);
+    shutdown_completed_rx.recv().await;
+    println!("shutdown has completed");
 
     Ok(())
 }
 
 impl Listener {
-    pub async fn new(addr: &str, shutdown_broadcast: broadcast::Sender<()>) -> Result<Listener> {
+    pub async fn new(
+        addr: &str,
+        shutdown_broadcast: broadcast::Sender<()>,
+        shutdown_completed_tx: mpsc::Sender<()>,
+    ) -> Result<Listener> {
         let listener = TcpListener::bind(addr).await?;
         let semaphore = Arc::new(Semaphore::new(MAX_LIMIT_CONNECTIONS));
         Ok(Listener {
             listener,
             semaphore,
             shutdown_broadcast,
+            shutdown_completed_tx,
         })
     }
 
     pub async fn run(&mut self) -> Result<()> {
-        let db = DbHolder::new();
+        let db = DbHolder::new(
+            self.shutdown_broadcast.subscribe(),
+            self.shutdown_completed_tx.clone(),
+        );
         loop {
             let permit = self.semaphore.clone().acquire_owned().await.unwrap();
 
@@ -62,6 +82,7 @@ impl Listener {
                 connection: Connection::new(socket),
                 db: db.clone(),
                 shutdown_receiver: self.shutdown_broadcast.subscribe(),
+                _shutdown_completed_tx: self.shutdown_completed_tx.clone(),
             };
 
             spawn(async move {
@@ -74,17 +95,10 @@ impl Listener {
             });
         }
     }
-
-    fn shutdown(&self) -> Result<()> {
-        println!("send signal");
-        self.shutdown_broadcast.send(())?;
-        Ok(())
-    }
 }
 
 impl Handler {
     async fn run(&mut self) -> Result<()> {
-        // TODO: break out the loop when the server shutdown
         loop {
             let frame = select! {
                 frame = self.connection.read_frame() => frame?,
